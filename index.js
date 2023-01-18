a36a00fb52269f148da8bb0a21f1b7447add1426
michaelcchu <50280000+michaelcchu@users.noreply.github.com>
2023-01-18 13:00:48 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 function start() {
     const tk = new verovio.toolkit();
     console.log("Verovio has loaded!");
+    tk.setOptions({
+        scale: 50,
+        landscape: true,
+        adjustPageWidth: true,
+    });
+    console.log("Verovio options:", tk.getOptions());
+    console.log("Verovio default options:", tk.getDefaultOptions());
     fetch("https://www.verovio.org/examples/downloads/Schubert_Lindenbaum.mei")
     .then( (response) => response.text() )
     .then( (meiXML) => {
